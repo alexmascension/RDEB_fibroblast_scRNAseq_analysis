bb27515e6c2c57d200601d58d23fede161dadf99
Alex M. Ascensión <alexmascension@gmail.com>
2022-11-15 17:46:33 +0100

    Added plot_WT_KO_genes func

--- functions.py
+++ functions.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from scipy.stats import chi2_contingency
 import numpy as np
 import scanpy as sc
 import seaborn as sns
 from statannot import add_stat_annotation
 
+magma = [plt.get_cmap('magma')(i) for i in np.linspace(0,1, 80)]
+magma[0] = (0.88, 0.88, 0.88, 1)
+magma = mpl.colors.LinearSegmentedColormap.from_list("", magma[:65])
 
-dict_WT_KO_colors = {'KO1': '#ea693c', 'KO2': '#f28d5c', 'WT1': '#20668d', 'WT2': '#229eb2', 'KO': '#ee7b4c', 'WT': '#2182a0'}
+
+dict_WT_KO_colors = {'KO1': '#f07c44', 'KO2': '#bf480f', 'WT1': '#409ed3', 'WT2': '#1c5778', 'KO': '#ee7b4c', 'WT': '#2182a0'}
 
 
 def adata_plot_KOvsWT(adata, list_names, do_return=False, col_cell_type='merged_cell_type'):
@@ -82,7 +87,8 @@ def adata_plot_KOvsWT(adata, list_names, do_return=False, col_cell_type='merged_
     
     
     
-def adata_plot_KOvsWT_2bars(adata, list_names, do_return=False, col_cell_type='merged_cell_type'):
+def adata_plot_KOvsWT_2bars(adata, list_names, do_return=False, col_cell_type='merged_cell_type', 
+                            add_number_to_label=False, sort_by='label'):
     fig, ax = plt.subplots(1, 1, figsize=(9, 6))
     
     df_counts_KO_WT = pd.DataFrame(columns=['KO1', 'KO2', 'WT1', 'WT2'], index=list_names)
@@ -108,10 +114,17 @@ def adata_plot_KOvsWT_2bars(adata, list_names, do_return=False, col_cell_type='m
                                                              counts_WT_all/len(adata) ]]))[1]
         
 
+    if sort_by=='counts':
+        idx_sort = ((df_counts_KO_WT['KO1'] + df_counts_KO_WT['KO2']) + 
+                (df_counts_KO_WT['WT1'] + df_counts_KO_WT['WT2'])).sort_values(ascending=False).index
+    elif sort_by=='diff':
+        idx_sort = ((df_counts_KO_WT['KO1'] + df_counts_KO_WT['KO2']) - 
+                (df_counts_KO_WT['WT1'] + df_counts_KO_WT['WT2'])).sort_values(ascending=False).index
+    elif sort_by=='label':
+        idx_sort = adata.obs[col_cell_type].cat.categories
+    else:
+        raise
 
-#     idx_sort = ((df_counts_KO_WT['KO1'] + df_counts_KO_WT['KO2']) + 
-#                 (df_counts_KO_WT['WT1'] + df_counts_KO_WT['WT2'])).sort_values(ascending=False).index
-    idx_sort = adata.obs['merged_cell_type'].cat.categories
 
     w, dx = 0.4, 0.2
     for idx, idx_name in enumerate(idx_sort):
@@ -157,7 +170,11 @@ def adata_plot_KOvsWT_2bars(adata, list_names, do_return=False, col_cell_type='m
     ax.set_ylim([0, 1.1 * max(list_max_val)])
     
     ax.set_xticks(range(len(df_counts_KO_WT)))
-    ax.set_xticklabels([i.split(': ')[1] for i in idx_sort], rotation=90)
+    
+    if add_number_to_label:
+        ax.set_xticklabels([i for i in idx_sort], rotation=90)
+    else:
+        ax.set_xticklabels([i.split(': ')[1] for i in idx_sort], rotation=90)
 
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
@@ -195,3 +212,51 @@ def stat_annot_gene(gene, adata, dict_pops, type_plot='violin', add_stats=True):
             except:
                 pass
 
+
+def plot_WT_KO_genes(adata, genes, n_cols=3, figsize=None, plot_labels_batch=True, plot_KO_vs_WT=True):
+    p_p = plot_labels_batch + plot_KO_vs_WT
+    
+    adata_WT, adata_KO = adata[adata.obs['batch'].isin(['WT1', 'WT2'])], adata[adata.obs['batch'].isin(['KO1', 'KO2'])]
+    adata_WT.uns['batch_colors'] = [dict_WT_KO_colors['WT1'], dict_WT_KO_colors['WT2']]
+    adata_KO.uns['batch_colors'] = [dict_WT_KO_colors['KO1'], dict_WT_KO_colors['KO2']]
+    
+    n_rows = int((len(genes) + p_p) / n_cols) + ((len(genes) + p_p) % n_cols > 0)
+    
+    if figsize is None:
+        figsize = (n_cols * 4, n_rows * 2 * 2.5)
+    
+    fig, axs = plt.subplots(n_rows * 2, n_cols, figsize=figsize)
+    
+    start = 0
+    if plot_labels_batch:
+        sc.pl.umap(adata, color='batch', frameon=False, cmap=magma, title='batch',
+                   show=False, ax=axs[1][start])
+        sc.pl.umap(adata, color='subtype_number', frameon=False, cmap=magma, show=False, title='Subtype',
+                   legend_fontoutline=4, legend_fontsize='x-large', legend_loc='on data', ax=axs[0][start])
+        start += 1
+        
+    if plot_KO_vs_WT:
+        sc.pl.umap(adata_WT, color='batch', frameon=False, cmap=magma, title='WT',
+                   show=False, ax=axs[0][start])
+        sc.pl.umap(adata_KO, color='batch', frameon=False, cmap=magma, title='KO',
+                   show=False, ax=axs[1][start])
+    
+    for idx, gene in enumerate(genes, start=p_p):
+        row_gene = int(idx / n_cols) 
+        col_gene = idx % n_cols
+        
+        min_gene, max_gene = adata_fb[:, gene].X.min(), adata_fb[:, gene].X.max()
+        
+        sc.pl.umap(adata_WT, color=gene, frameon=False, cmap=magma, use_raw=False,
+                   show=False, legend_loc='on data', ax=axs[row_gene * 2][col_gene], vmin=min_gene, vmax=max_gene)
+        
+        sc.pl.umap(adata_KO, color=gene, frameon=False, cmap=magma, use_raw=False,
+                   show=False, legend_loc='on data', ax=axs[row_gene * 2 + 1][col_gene], vmin=min_gene, vmax=max_gene)        
+    
+    
+    # clean axes
+    for col in range(col_gene + 1, n_cols):
+        axs[row_gene * 2][col].set_axis_off()
+        axs[row_gene * 2 + 1][col].set_axis_off()
+    
+    plt.tight_layout()
